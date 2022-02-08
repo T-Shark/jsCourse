1f7721ff68c9bea9a925e76bf1ce80c9c9af51ea
Todor Sharkov <99077477+T-Shark@users.noreply.github.com>
2022-02-08 09:35:53 +0200

    Update script.js

--- Form/script.js
+++ Form/script.js
@@ -1,11 +1,20 @@
 const availableFilters = ["Color_Input","Text_Input","Digits_Input"]
 
+function makeBoxSelect(){
+    const box=document.createElement("div")
+    box.id="boxSelect"
+    box.style="display:flex;flex-direction:column;"
+
+    return box
+}
+
 function makeSelect() {
     const select = document.createElement("select")
     const defaultOption = document.createElement("option")
 
     select.name = "select"
-    select.style="margin-bottom:50px"
+    select.id = "select"
+    select.style="margin-bottom:20px"
 
     defaultOption.value = "-1"
     defaultOption.innerHTML = "Please select type of input"
@@ -32,36 +41,15 @@ function drawLabels(name){
 	return label
 }
 
-function makeTextInput(){
+function makeInput(whatKind){
 
-	const txt_input=document.createElement("input")
+    const inputche=document.createElement("input")
 
-	txt_input.type="text"
-	txt_input.name="text"
+    inputche.type = whatKind
+    inputche.value = whatKind
 
-	
-	return txt_input
-}
-
-function makeDigitsInput(){
-
-	const num_input=document.createElement("input")
-
-	num_input.type="number"
-	num_input.name="number"
-
-	
-	return num_input
-}
-
-function makeColorInput(){
-	const color_input=document.createElement("input")
-
-	color_input.type="color"
-	color_input.name="color"
-
-	
-	return color_input
+    
+    return inputche
 }
 
 function makeForm(){
@@ -76,32 +64,58 @@ function drawItems(parent, children) {
     children.forEach(x => parent.appendChild(x))
 }
 
+function butSubmit(){
+    const but=document.createElement("button")
+    but.id="submitNow"
+    but.style="margin-bottom:50px"
+    but.innerHTML="Click here to create input"
+
+    return but
+}
+
 function main(){
 	const board=document.getElementById("board");
-	
+   
 
-	drawItems(board, [makeSelect()])
+    drawItems(board, [makeBoxSelect()])
+	const boxSelect=document.getElementById("boxSelect")
+
+	drawItems(boxSelect, [makeSelect()])
+
+    drawItems(boxSelect, [drawLabels("Give a name of the input")])
+
+    drawItems(boxSelect, [makeInput("text")])
+
+    drawItems(boxSelect, [butSubmit()])
 
 	drawItems(board, [makeForm()])
 
-	board.addEventListener("change", function (event) {
+    const inputText= document.getElementsByTagName("input")[0]
+    inputText.value=""
 
-        const { value } = event.target
-        const formata=document.getElementById("formata");
+    const butSub=document.getElementById("submitNow");
 
-        if (value === "Color_Input") {
-        	drawItems(formata, [drawLabels(value)])
-            drawItems(formata, [makeColorInput()])
+        butSub.addEventListener("click", function (event) {
+
+        const sl=document.getElementById("select");
+
+
+        if (sl.options[sl.selectedIndex].value === "Color_Input" && inputText.value !="") {
+        	drawItems(formata, [drawLabels(inputText.value)])
+            drawItems(formata, [makeInput("color")])
         }
 
-        if (value === "Text_Input") {
-        	drawItems(formata, [drawLabels(value)])
-            drawItems(formata, [makeTextInput()])
+        if (sl.options[sl.selectedIndex].value === "Text_Input" && inputText.value !="") {
+        	drawItems(formata, [drawLabels(inputText.value)])
+            drawItems(formata, [makeInput("text")])
         }
 
-         if (value === "Digits_Input") {
-         	drawItems(formata, [drawLabels(value)])
-             drawItems(formata, [makeDigitsInput()])
+         if (sl.options[sl.selectedIndex].value === "Digits_Input" && inputText.value !="") {
+         	drawItems(formata, [drawLabels(inputText.value)])
+             drawItems(formata, [makeInput("number")])
+        }
+        if (sl.options[sl.selectedIndex].value === "-1" || inputText.value ===""){
+            alert("Make sure that you have selected any input type and you wrote some name for it!!!")
         }
 
     })
